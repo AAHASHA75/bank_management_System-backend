8e89057fc150d9a0b9705945914dfa116196aeb3
Aahash <aahashkittusamy@gmail.com>
2026-03-14 12:02:31 +0530

    removed all security folders and file and added docker file

--- Bank-Management-System2/src/main/java/com/example/Bank_Management_System2/Configuration/SecurityConfiguration.java
+++
@@ -1,8 +0,0 @@
-package com.example.Bank_Management_System2.Configuration;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@
-public class SecurityConfiguration {
-}
