f65f536bd4ff1066ecaaf9a1839e28c8fcb36755
Aahash <aahashkittusamy@gmail.com>
2026-04-07 14:31:49 +0530

    for deployment last1

--- Bank-Management-System2/repo/src/main/java/com/example/Bank_Management_System2/CorsConfig.java
+++ Bank-Management-System2/repo/src/main/java/com/example/Bank_Management_System2/CorsConfig.java
@@ -2,20 +2,21 @@ package com.example.Bank_Management_System2;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
 import org.springframework.web.servlet.config.annotation.*;
 
 @Configuration
 public class CorsConfig {
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
+
             @Override
-            public void addCorsMappings(CorsRegistry registry) {
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins(
-                                "https://bank-management-system2-frontend.vercel.app"
-                        )
-                        .allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
+                        .allowedOrigins("https://bank-management-system2-frontend.vercel.app")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
             }
