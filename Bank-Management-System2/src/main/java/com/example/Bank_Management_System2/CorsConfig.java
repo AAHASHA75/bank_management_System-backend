4ad2639c1ea9eb79e6943f18126b74640924083a
Aahash <aahashkittusamy@gmail.com>
2026-04-07 09:49:26 +0530

    prepare for production deployment

--- Bank-Management-System2/src/main/java/com/example/Bank_Management_System2/CorsConfig.java
+++ Bank-Management-System2/src/main/java/com/example/Bank_Management_System2/CorsConfig.java
@@ -2,22 +2,23 @@ package com.example.Bank_Management_System2;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.*;
 
 @Configuration
 public class CorsConfig {
-        @Bean
-        public WebMvcConfigurer corsConfigurer() {
-            return new WebMvcConfigurer() {
-                @Override
-                public void addCorsMappings(CorsRegistry registry) {
-                    registry.addMapping("/**")
-                            .allowedOrigins("*") // allow all origins for development
-                            .allowedMethods("*")
-                            .allowedHeaders("*");
-                }
-            };
-        }
-}
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("https://bank-management-system2-frontend.vercel.app")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
